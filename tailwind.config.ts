bbe75ceae402ae4320b7789cadd0b42e61f27a77
Kenneth <kendevops@gmail.com>
2023-11-17 09:46:36 +0100

    initial: setup of fonts for the whole application

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        monument: "var(--font-monument)",
+        satoshi: "var(--font-satoshi)",
+      },
     },
     screens: {
       xs: "360px",
@@ -24,4 +28,4 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config
+export default config;
