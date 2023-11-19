11fd5f5e830e9bca41d327ad35227ac35120cb9e
Kenneth <kendevops@gmail.com>
2023-11-19 08:55:57 +0100

    fix: profiting and creon pass image and video bg blend

--- src/components/layouts/Profiting.tsx
+++ src/components/layouts/Profiting.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Profiting() {
   return (
-    <section className="mt-[120px] px-[190px]">
+    <section className="pt-[120px] pb-[141px] px-[190px]">
       <h1 className="text-white font-monument text-[68px] font-normal uppercase leading-[110%]">
         Profiting Through
       </h1>
       <h2 className="bg-linear bg-clip-text text-transparent font-normal font-monument text-right text-[38px] leading-[120%] uppercase mb-[83px]">
         AI Innovation & Decentralization
       </h2>
-      <div className="flex gap-[71px] mb-[141px] relative">
+      <div className="flex gap-[71px]  relative">
         <Image
           src="/Images/Normal_bitrate.png"
           alt="Creon Moon Image"
@@ -18,8 +18,7 @@ export default function Profiting() {
           height={485}
           priority
         />
-        <div className="absolute w-[947px] h-[485px] rounded-md inset-0">
-          <div className="absolute inset-0 rounded-md bg-hero-pattern mix-blend-soft-light"></div>
+        <div className="absolute w-[947px] h-[485px] rounded-md inset-0 bg-image-pattern mix-blend-soft-light">
         </div>
         <div className="flex flex-col justify-center items-center">
           <h4 className="mb-[28px] text-white font-satoshi font-bold text-[22px] leading-[130%]">
--- src/components/molecules/CreonPassRight.tsx
+++ src/components/molecules/CreonPassRight.tsx
@@ -7,13 +7,15 @@ export default function CreonPassRight() {
         autoPlay
         loop
         muted
-        className="flex-shrink-0 bg-video-pattern"
+        className="flex-shrink-0"
       >
         <source
           src="https://s3-figma-videos-production-sig.figma.com/video/935630556139161368/TEAM/2f74/56c3/-b645-4e00-ba7a-db1f08b78d66?Expires=1701043200&Signature=YTG9kSMrNVEZul~WgmxHzbS6yw-gA~lQpRygCKOkEWrGZzt91vTWjqS65hPr88V5nd2Jf84JjnSfVf3VSjX3~fjNh9O3rASZ~PiP3Vm1rPADzymyy5trRSdL6qrNUk-UVqq8zx8jSc6NOibGPLNpBVzRvmmA4ja1GUwVzC5jAICOujdwfT71SfVweI-Jihmr9NxPFE9jOIRP7c39EdG6z221SxpnFCiS7hvX4fxPpOtiD0Kv0MbAAZ5N4UeoHskZGI2Tx8Q2E~jMhkyk4guqTLarHA4sb1cxCNUUv8BASGOwkHbU0hJWck3AXGaxRANr~6-E5qaF9aHZo-GH6E5FpQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
           type="video/mp4"
         />
       </video>
+      <div className="absolute w-[808px] h-[808px] rounded-md inset-0 bg-video-pattern mix-blend-soft-light">
+        </div>
       {/* Ellipse at the top */}
       <div className="absolute top-0 left-[80%] transform -translate-x-1/2 -translate-y-1/2 w-[1288px] h-[134px] bg-black rounded-[1288px] overflow-hidden"></div>
 
