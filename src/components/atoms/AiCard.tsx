6cb88ca3810abc22a3d00e45baeaa0ed8876a54c
Kenneth <kendevops@gmail.com>
2023-11-21 04:56:07 +0100

    fix: fixed responsiveness for Coming ai component screen size 375px

--- src/components/atoms/AiCard.tsx
+++ src/components/atoms/AiCard.tsx
@@ -3,17 +3,36 @@ import { CardType } from "./Token";
 
 export default function AiCard({ title, description, imgSrc }: CardType) {
   return (
-    <article className="flex xl:h-[302px] xl:w-[1440px]  rounded-md bg-CardColor">
-      <div className="flex flex-col py-[24px] pl-[24px] pr-[30px]  xl:py-10 xl:px-10">
-        <h1 className="text-white font-monument text-[28px] xl:text-[38px] font-normal uppercase leading-[130%]">
+    <article className="flex flex-col-reverse md:flex-row xl:h-[302px] xl:w-[1440px]  rounded-md bg-CardColor">
+      <div className="flex flex-col py-[24px] md:pl-[24px] md:pr-[30px]  xl:py-10 xl:px-10 w-[256px] md:w-auto">
+        <h1 className="text-white font-monument text-[22px] md:text-[28px] xl:text-[38px] font-normal uppercase leading-[130%]">
           {title}
         </h1>
-        <p className="text-white font-satoshi text-base xl:text-lg font-normal">
+        <p className="text-white font-satoshi self-stretch text-base xl:text-lg font-normal text-ellipsis overflow-hidden">
           {description}
         </p>
       </div>
-      <Image src={imgSrc} alt="Ai Image" width={453} height={302} className="flex-shrink-0 xl:block hidden" />
-      <Image src={imgSrc} alt="Ai Image" width={308} height={260} className="flex-shrink-0 xl:hidden block" />
+      <Image
+        src={imgSrc}
+        alt="Ai Image"
+        width={453}
+        height={302}
+        className="flex-shrink-0 xl:block hidden"
+      />
+      <Image
+        src={imgSrc}
+        alt="Ai Image"
+        width={308}
+        height={260}
+        className="flex-shrink-0 hidden md:block"
+      />
+      <Image
+        src={imgSrc}
+        alt="Ai Image"
+        width={292}
+        height={169}
+        className="flex-shrink-0 md:hidden block"
+      />
     </article>
   );
 }
