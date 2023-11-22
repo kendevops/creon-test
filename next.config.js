87d56e83389cc42c956c9542116c5c437edd24bd
Kenneth <kendevops@gmail.com>
2023-11-22 08:52:06 +0100

    fix: removed the next-videos dependency i installed for the background video since vercel doesn't accept static file path

--- next.config.js
+++ next.config.js
@@ -3,19 +3,3 @@ const nextConfig = {}
 
 module.exports = nextConfig
 
-const withVideos = require("next-videos");
-
-module.exports = withVideos();
-
-// module.exports = {
-//   videos: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "s3-figma-videos-production-sig.figma.com",
-//         port: "",
-//         pathname: "/video/**",
-//       },
-//     ],
-//   },
-// };
